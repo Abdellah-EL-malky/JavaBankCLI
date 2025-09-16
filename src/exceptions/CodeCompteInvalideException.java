a54501401227f0eefa7f479095840a796df55967
Abdellah <abdel.elmalky@gmail.com>
2025-09-16 16:31:02 +0100

    Mise en place des exceptions custom

---
+++ src/exceptions/CodeCompteInvalideException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class CodeCompteInvalideException extends Exception {
+    public CodeCompteInvalideException(String message) {
+        super(message);
+    }
+}
---
+++ src/exceptions/MontantNegatifException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class MontantNegatifException extends Exception {
+    public MontantNegatifException(String message) {
+        super(message);
+    }
+}
