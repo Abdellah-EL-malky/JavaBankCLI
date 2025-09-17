d2084ddc8823f72a7c130a8fbf3dbc6fca46a102
Abdellah <abdel.elmalky@gmail.com>
2025-09-17 10:00:20 +0100

    Fin de l'Architecture et Structure de Base

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+import utilitaire.DateUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Date: " + DateUtils.formaterDate(DateUtils.obtenirDateActuelle()));
+    }
+}
